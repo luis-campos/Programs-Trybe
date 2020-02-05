a77324818eb59d779dd0c2deda2eb17029d67a44
luis.campos <gucampos845@gmail.com>
2020-02-05 14:15:15 -0300

    Atualização dos nomes dos arquivos

--- exercicio3.sh
+++ exercicio3.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
+nome=`hostname`
 
-path="/home/luis-campos/unix-tests/countries.txt"
-
-if [ -e "$path" ]
-
-then echo "O caminho $path está habilitado!"
-
-else echo "Caminho inexistente"
-
-fi
-
-if [ -x "$path" ] 
-then echo "Você tem permissão para editar $path"
-
-else echo "Você NÃO foi autorizado a editar $path"
-
-fi
+echo "Este script está rodando no computador: " $nome
